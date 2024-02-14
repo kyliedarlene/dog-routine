f08ebcc35bc966923dcb521327b78eed807a2665
Kylie Clark <kyliedarlene@icloud.com>
2024-02-14 11:29:52 -0700

    initial commit

---
+++ server/migrations/versions/e03c8fa7461d_initial_db.py
@@ -0,0 +1,24 @@
+"""initial db
+
+Revision ID: e03c8fa7461d
+Revises: 
+Create Date: 2024-02-14 11:29:50.025507
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e03c8fa7461d'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
